e01608798a1086ff624688eb6d255745160e09b3
tobinio <Tobias.frischmann1@gmail.com>
2025-08-05 20:23:25 +0200

    remove .description() in tests

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,10 +1,8 @@
 import { expect, test } from "@nuxt/test-utils/playwright";
 import { setAuthCookie } from "./utils";
 
-test.describe("general", () => {
-  test("has title", async ({ page, goto }) => {
-    await setAuthCookie(page);
-    await goto("/", { waitUntil: "hydration" });
-    await expect(page).toHaveTitle("TaskLine");
-  });
+test("has title", async ({ page, goto }) => {
+  await setAuthCookie(page);
+  await goto("/", { waitUntil: "hydration" });
+  await expect(page).toHaveTitle("TaskLine");
 });
