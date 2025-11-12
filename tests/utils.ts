8a88ab59e0ba27603c6eba2cdd6fc1af97f61f5f
ToBinio <tobias.frischmann1@gmail.com>
2025-11-12 15:01:40 +0100

    mark e2e tests as not working

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,26 +1,15 @@
 import { expect } from "@nuxt/test-utils/playwright";
 import type { Locator, Page } from "@playwright/test";
 import { v4 } from "uuid";
-import { Auth } from "~~/shared/auth";
 
 export async function setAuthCookie(page: Page) {
-	await page.context().addCookies([
-		{
-			name: "token",
-			value: Auth.createRaw(
-				v4(),
-				"test@example.com",
-				"",
-				"30s",
-				process.env.JWT_SECRET!,
-			),
-			domain: "127.0.0.1",
-			path: "/",
-			httpOnly: false,
-			secure: false,
-			sameSite: "Lax",
-		},
-	]);
+	throw new Error("not implemented");
+
+	//await page.context().addCookies([
+	//{
+	//	name: "token",
+	//	value: "",
+	//});
 }
 
 export async function addTodo(page: Page, title: string, note?: string) {
