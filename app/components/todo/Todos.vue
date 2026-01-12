e990913abf62005ba2ecd6d8a7d7ad7e68e47af1
ToBinio <tobias.frischmann1@gmail.com>
2026-01-12 15:08:15 +0100

    add drag and drop result indicator

--- app/components/todo/Todos.vue
+++ app/components/todo/Todos.vue
@@ -1,26 +1,34 @@
 <script setup lang="ts">
 	import { useFilteredTodos } from "~/composables/useFilteredTodos";
 
-	const filterdTodos = useFilteredTodos();
+	const filteredTodos = useFilteredTodos();
 
-	const { startDrag, endDrag, dropOnTodoHandler } = useTodoDragging();
+	const {
+		startDrag,
+		endDrag,
+		dropOnTodoHandler,
+		dragLeave,
+		dragOver,
+		draggedTodo,
+		hoveredTodo,
+		hoveredState,
+	} = useTodoDragging();
 </script>
 
 <template>
 	<div class="flex justify-center" data-testid="todos-container">
 		<div
-			class="max-h-[calc(100dvh-5.75rem)] w-dvw max-w-200 overflow-auto px-1 md:max-h-[calc(100dvh-2.5rem)]"
+			class="max-h-[calc(100dvh-5.75rem)] w-dvw max-w-200 overflow-auto px-1 md:max-h-[calc(100dvh-2.5rem)] pb-1"
 		>
 			<TransitionGroup
 				leave-to-class="max-h-0! opacity-0 scale-y-0"
 				enter-from-class="max-h-0! opacity-0 scale-y-0"
 			>
-				<Todo
-					v-for="todo in filterdTodos"
+				<div
+					class="transition-all duration-300 relative"
+					v-for="todo in filteredTodos"
 					:key="todo.uuid"
 					draggable="true"
-					class="max-h-18 transition-all duration-300 data-[dragged]:blur-[1px] data-[dragged]:grayscale-100"
-					:data="todo"
 					@dragstart="
             (event: DragEvent) => {
               startDrag(event, todo.uuid);
@@ -29,15 +37,31 @@
 					@dragend="endDrag"
 					@dragover="
             (event: DragEvent) => {
-              event.preventDefault();
+              dragOver(event, todo.uuid);
             }
           "
+					@dragleave="dragLeave"
 					@drop="
             (event: DragEvent) => {
               dropOnTodoHandler(event, todo.uuid);
             }
           "
-				/>
+				>
+					<div
+						v-if="hoveredState === 'above' && hoveredTodo === todo.uuid && draggedTodo !== todo.uuid"
+						class="h-0.5 absolute w-full bg-primary-hover rounded-full"
+					/>
+					<Todo
+						:data-dragged="draggedTodo === todo.uuid"
+						:data-hovered="hoveredTodo === todo.uuid"
+						class="max-h-18 transition-all duration-300 data-[dragged=true]:blur-[1px] data-[dragged=true]:grayscale-100"
+						:data="todo"
+					/>
+					<div
+						v-if="hoveredState === 'below' && hoveredTodo === todo.uuid && draggedTodo !== todo.uuid"
+						class="h-0.5 absolute w-full bg-primary-hover rounded-full"
+					/>
+				</div>
 			</TransitionGroup>
 		</div>
 	</div>
--- app/composables/useTodoDragging.ts
+++ app/composables/useTodoDragging.ts
@@ -4,13 +4,46 @@ import type { UUID } from "~~/shared/types";
 export function useTodoDragging() {
 	const todoStore = useTodoStore();
 
+	const draggedTodo = ref<UUID | null>(null);
+	const hoveredTodo = ref<UUID | null>(null);
+	const hoveredState = ref<"above" | "below" | null>(null);
+
 	function startDrag(event: DragEvent, uuid: UUID) {
 		event.dataTransfer?.setData("uuid", uuid);
-		(event.target as HTMLElement).dataset.dragged = "true";
+		draggedTodo.value = uuid;
 	}
 
-	function endDrag(event: DragEvent) {
-		delete (event.target as HTMLElement).dataset.dragged;
+	function endDrag() {
+		draggedTodo.value = null;
+		hoveredTodo.value = null;
+		hoveredState.value = null;
+	}
+
+	function dragOver(event: DragEvent, uuid: UUID) {
+		event.preventDefault();
+
+		hoveredTodo.value = uuid;
+
+		const hoveredIndex = todoStore.data.findIndex(
+			(todo) => todo.uuid === hoveredTodo.value,
+		);
+		const draggedIndex = todoStore.data.findIndex(
+			(todo) => todo.uuid === draggedTodo.value,
+		);
+
+		if (hoveredIndex === -1 || draggedIndex === -1) return;
+
+		if (hoveredIndex > draggedIndex) {
+			hoveredState.value = "below";
+		} else {
+			hoveredState.value = "above";
+		}
+	}
+
+	function dragLeave(event: DragEvent) {
+		event.preventDefault();
+		hoveredTodo.value = null;
+		hoveredState.value = null;
 	}
 
 	async function dropOnTodoHandler(event: DragEvent, uuid: UUID) {
@@ -49,5 +82,10 @@ export function useTodoDragging() {
 		endDrag,
 		dropOnTodoHandler,
 		dropOnDayHandler,
+		dragOver,
+		dragLeave,
+		draggedTodo,
+		hoveredTodo,
+		hoveredState,
 	};
 }
