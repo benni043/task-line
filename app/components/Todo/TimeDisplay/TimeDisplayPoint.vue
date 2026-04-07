89f4528e3b8aa19e414c58521daaa7499227a9d0
ToBinio <tobias.frischmann@proton.me>
2026-04-07 14:29:09 +0200

    simplify code

--- app/components/Todo/TimeDisplay/TimeDisplayPoint.vue
+++ app/components/Todo/TimeDisplay/TimeDisplayPoint.vue
@@ -5,64 +5,32 @@
 
 	const { point } = defineProps<{ point: TimePoint }>();
 
-	const timeFrameString = computed(() => {
-		return point.time;
-	});
-
 	const time = computed(() => {
-		let time = parseAbsoluteToLocal(point.time).toDate().getTime();
-
-		return {
-			start: time - 1000 * 60 * 30,
-			end: time + 1000 * 60 * 30,
-		};
-	});
-
-	function capTime(time: number): number {
-		return Math.min(
-			Math.max(time, sanitizeDate(new Date()).getTime()),
-			addDays(sanitizeDate(new Date()), 7).getTime(),
-		);
-	}
-
-	const cappedTime = computed(() => {
-		return {
-			start: capTime(time.value.start),
-			end: capTime(time.value.end),
-		};
-	});
-
-	const width = computed(() => {
-		const timeDiff = cappedTime.value.end - cappedTime.value.start;
-		const numberOfDays = timeDiff / (24 * 60 * 60 * 1000.0);
-		const percentage = numberOfDays / 7.0;
-		return percentage;
+		return parseAbsoluteToLocal(point.time).toDate().getTime();
 	});
 
 	const offset = computed(() => {
 		const now = new Date();
 		now.setHours(0, 0, 0, 0);
 
-		const timeDiff = cappedTime.value.start - now.getTime();
+		const timeDiff = time.value - now.getTime();
 		const numberOfDays = timeDiff / (24 * 60 * 60 * 1000.0);
 		const percentage = numberOfDays / 7.0;
+
 		return percentage;
 	});
 
 	const isCappedRight = computed(() => {
-		return time.value.end > addDays(sanitizeDate(new Date()), 7).getTime();
+		return time.value > addDays(sanitizeDate(new Date()), 7).getTime();
 	});
 
 	const isCappedLeft = computed(() => {
-		return time.value.start < sanitizeDate(new Date()).getTime();
+		return time.value < sanitizeDate(new Date()).getTime();
 	});
 </script>
 
 <template>
-	<div
-		class="relative flex h-2 flex-col justify-center"
-		:title="timeFrameString"
-	>
+	<div class="relative flex h-2 flex-col justify-center" :title="point.time">
 		<div
 			class="absolute top-1/2 flex h-2 w-full -translate-y-1/2 justify-evenly"
 		>
@@ -73,9 +41,9 @@
 			/>
 		</div>
 		<div
-			class="bg-primary absolute top-1/2 h-1 -translate-y-1/2 rounded-full"
+			v-if="!isCappedLeft && !isCappedRight"
+			class="bg-primary absolute top-1/2 h-2 w-2 -translate-y-1/2 rounded-full"
 			:style="{
-        width: `${width * 100}%`,
         left: `${offset * 100}%`,
       }"
 		/>
